a38f06b6425459bca4d2aef20592cf82bd7b6a49
alexandre cousin <cousin.a@laposte.net>
2019-02-12 15:20:34 +0100

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Read
+  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # Create
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
+  # Update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update', as: :update
+
+  # Delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
